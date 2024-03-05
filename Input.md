59ff87c8de267bf5f1e6e1d7d48ad4e46ed78213
iClaw <ilya.timakov24@gmail.com>
2024-03-05 20:35:50 +0700

    Input+Output+Code

---
+++ Input.md
@@ -0,0 +1,1 @@
+SyntaxError: errorsawww invalid syntax
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, World")
+# Working With Files
+f = open("Input.txt")
+InputData = []
+for line in f:
+    InputData.append()
+
+f.close()
+f = open("Output.txt", 'w')
+f.write("OutputData")
+f.close()
